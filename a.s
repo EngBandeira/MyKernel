efa510d881eb3042415f75bbbe995004ef8e3fca
Eduardo Bandeira Palhano <eduardo.bandeira.eng@outlook.com>
2026-02-28 03:37:50 -0300

    Keyboard Working

---
+++ a.s
@@ -0,0 +1,48 @@
+	.file	"a.c"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$16, %esp
+	call	__x86.get_pc_thunk.ax
+	addl	$_GLOBAL_OFFSET_TABLE_, %eax
+	movw	$96, -2(%ebp)
+	movzwl	-2(%ebp), %eax
+#APP
+# 5 "src/a.c" 1
+	movw %ax, %dx
+	inb %dx, %al
+	movb %al, %al
+# 0 "" 2
+#NO_APP
+	movb	%al, -3(%ebp)
+	movl	$0, %eax
+	movl	%ebp, %esp
+	.cfi_def_cfa_register 4
+	popl	%ebp
+	.cfi_restore 5
+	.cfi_def_cfa_offset 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.section	.text.__x86.get_pc_thunk.ax,"axG",@progbits,__x86.get_pc_thunk.ax,comdat
+	.globl	__x86.get_pc_thunk.ax
+	.hidden	__x86.get_pc_thunk.ax
+	.type	__x86.get_pc_thunk.ax, @function
+__x86.get_pc_thunk.ax:
+.LFB1:
+	.cfi_startproc
+	movl	(%esp), %eax
+	ret
+	.cfi_endproc
+.LFE1:
+	.ident	"GCC: (GNU) 15.2.1 20260209"
+	.section	.note.GNU-stack,"",@progbits
--- src/start.s
+++ src/start.s
@@ -4,12 +4,16 @@
 .set MAGIC,    0x1BADB002       /* 'magic number' lets bootloader find the header */
 .set CHECKSUM, -(MAGIC + FLAGS) /* checksum of above, to prove we are multiboot */
 .set KEYBOARD_TIMEOUT,   200
+.set RW_P2_DATA_PORT,   0x60
+.set R_STATUS_REGISTER,   0x64
+.set W_COMMAND_REGISTER,   0x64
+.set ACK_KEYBOARD,   0xFA
+.set RESEND_KEYBOARD,   0xFE
 .section .multiboot
 .align 4
 .long MAGIC
 .long FLAGS
 .long CHECKSUM
-
 
 
 .section .data
@@ -54,49 +58,17 @@ _keyboard_int:
     # call keyboard_event
     # movl $0xD0, 0x64
     # movl 60, %eax
-    pushl $MESSAGE
-    call vga_str_put
-    popl %eax
+    # pushl $MESSAGE
+    # call vga_str_put
+    # popl %eax
+    call keyboard_handler
+    # movb $0x20, %al
+    # outb %al, $0x20
     popa
     iret
 
 
 
-_send_keyboard_command:
-    pushl %ebp
-    movl %esp, %ebp
-    xor %ebx, %ebx
-.loopaa:
-    cmp $KEYBOARD_TIMEOUT, %ebx
-    jg _error
-    inc %ebx
-    inb $0x64, %al
-    and $1<<1, %al
-    cmp $0, %al
-    jne .loopaa
-    movl 8(%ebp), %edx # 8 + %ebp
-
-    movl %edx, %ebx
-.loop2:
-    movl 8(%ebp, %ebx, 4), %eax # 8 + %ebp + 4*%edx
-.loop_resend:
-    outl %eax, $0x60
-
-    inb $0x60, %al
-    cmpb $0xfe, %al
-    je .loop_resend
-
-    decl %ebx
-    cmp $0, %ebx
-    jne .loop2
-
-    # movl 12(%ebp), %eax
-    # movl 16(%ebp), %eax
-
-
-    nop
-    popl %ebp
-    ret
 
 flush_gdt:
     pushl %ebp
@@ -110,6 +82,16 @@ flush_gdt:
     lgdt (%esp)
 
     add $6, %esp
+
+    mov $0x10, %ax      # data selector (GDT entry 2 -> 2*8 = 0x10)
+    mov %ax, %ds
+    mov %ax, %es
+    mov %ax, %fs
+    mov %ax, %gs
+    mov %ax, %ss
+
+    jmp $0x8, $.L1  # far jump to reload CS
+.L1:
 
     popl %ebp
     ret
@@ -136,112 +118,19 @@ flush_idt:
 _start:
     cli
     movl $stack_top, %esp
-    movl _keyboard_int, %eax # Store segment at 0x26
-    # lidt _IDT
-    # lgdt _GDT
-
-
-
-
-    movl $0xAA, %eax
-    outl %eax, $0x64 #Test
-    inb $0x60, %al
-    cmpb $0x55, %al
-    jne _error
-
-    movl $0xAB, %eax
-    outl %eax, $0x64 #Test
-    inb $0x60, %al
-    cmpb $0x0, %al
-    jne _error
-
-    movb $0b00011100, %al #Initialization Command Word 1
-    outb %al, $0x20
-    outb %al, $0xA0
-
-    movb $0, %al #Initialization Command Word 2
-    outb %al, $0x21
-    outb %al, $0xA1
-
-    movb $0b00000100, %al #Initialization Command Word Master 3
-    outb %al, $0x21
-    movb $0b00000100, %al #Initialization Command Word Slave 3
-    outb %al, $0xA1
-
-    movb $0, %al #All on in Interupt Mask Reg: OCW1
-    outb %al, $0x21
-    outb %al, $0xA1
-
-    movb $0b01100111, %al #All on in Interupt Commnad Reg: OCW2
-    outb %al, $0x20
-    outb %al, $0xA0
-
-    movb $0b00001100, %al #OCW3
-    outb %al, $0x20
-    outb %al, $0xA0
-
-
-    movl $0xAD, %eax #Disable First p2 port
-    outl %eax, $0x64 #Disable First p2 port
-
-    movl $0xA7, %eax #Disable Second p2 port
-    outl %eax, $0x64 #Disable Second p2 port
-
-    inl $0x60, %eax #Flush
-
-
-
-    mov $0b01000111, %eax #Set controller conf byte
-    outl %eax, $0x60 #Set controller conf byte
-
-    movl $0x60, %eax #Set controller conf byte
-    outl %eax, $0x64 #Set controller conf byte
-
-
-
-
-
-    movl $0xAE, %eax #Enable First p2 port
-    outl %eax, $0x64 #Enable First p2 port
-
-    movl $0xA8, %eax #Enable Second p2 port
-    outl %eax, $0x64 #Enable Second p2 port
-
-    push $0xF0
-    push $0x1
-    push $0x2
-    call _send_keyboard_command
-    addl $12, %esp
-
-    push $0xF4
-    push $0x1
-    call _send_keyboard_command
-    addl $8, %esp
 
     call kernel_init
 
     call flush_gdt
 
-    mov $0x10, %ax      # data selector (GDT entry 2 -> 2*8 = 0x10)
-    mov %ax, %ds
-    mov %ax, %es
-    mov %ax, %fs
-    mov %ax, %gs
-    mov %ax, %ss
-
-    jmp $0x08, $gdt_flush_done  # far jump to reload CS
-gdt_flush_done:
-
     call flush_idt
 
-    nop
+    sti
 
-    int $3
+    int $9
 
-    pushl $MESSAGE
-    call vga_str_put
-
-    addl $4, %esp
+    # movb 0xFE, %al
+    # outb  %al, 0x64
 
 
     call kernel_routine
